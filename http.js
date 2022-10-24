5a4421f89f9472b82daa486b890f6c5d4c96a12a
jonathanbarzola <jonabl2102@gmail.com>
2022-10-23 21:07:00 -0500

    Module HTTP learned

--- http.js
+++ http.js
@@ -1,8 +1,26 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer((request, response) => {
-    response.write('Hello World!');
-    response.end()
-}).listen(3000)
+const server = http.createServer((request, response) => {
+  console.log(request.url);
 
-console.log('Servidor escuchando en el puerto 3000');
+  if (request.url === "/") {
+    response.write("Welcome to the server!");
+    return response.end();
+  }
+
+  if (request.url === "/about") {
+    response.write("Welcome to the About!");
+    return response.end();
+  }
+
+  response.write(`
+    <h1>Not Found!</h1>
+    <p>Está página no se ha encontrado</>
+    <a href="/">Volver a la página principal</a>
+    `);
+  response.end();
+});
+
+server.listen(3000);
+
+console.log("Servidor escuchando en el puerto 3000");
