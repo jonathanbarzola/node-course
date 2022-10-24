c6fe4b421fc167e069df92b7fedc3a1832877faf
jonathanbarzola <jonabl2102@gmail.com>
2022-10-23 21:50:49 -0500

    learn node modules and npm

---
+++ cli.js
@@ -0,0 +1,15 @@
+const colors = require('colors');
+
+console.log('Hello World!'.bgGreen.strikethrough);
+console.log('faztweb.com'.bgRed);
+console.log('google.com'.bgYellow);
+
+
+/* 
+
+Las depencies son las dependencias que va a utilizar nuestro proyecto
+y que si se eliminan las podemos instalar todas nuevamente con el comando
+
+npm install
+
+*/
